30dcbdac4025816ebf30e65fb702cdeb5dbdabbe
Marsel Scheer <scheer@freescience.de>
2020-09-26 16:24:38 +0000

    implement resampling of Rn1 and the gof-test

--- R/gof_model.R
+++ R/gof_model.R
@@ -1,58 +1,3 @@
-## pseudo_gof_model <- function(data,
-##                       model,
-##                       m = 1000,
-##                       calc_beta_times_covariates
-##                       calc_Y_hat
-##                       get_Y_name
-##                       refit_model,
-##                       simulate_Y,
-##                       type = c("parametric", "wild"),
-##                       statistic = c("KS", "CvM")) {
-
-##   checkmate::assert_subset(type)
-##   checkmate::assert_subset(statistic)
-
-##   Y_name <- get_Y_name(model = model)
-##   if(checkmate::check_false(Y_name %in% names(data))) {
-##     stop(sprintf("According to get_Y_name() the name of Y should be %s. But cannot find it in the data-object"))
-##   }
-
-##   if (missing(calc_beta_times_covariates)) {
-##     calc_beta_times_covariates <- GOF:::calc_beta_times_covariates
-##   }
-##   metric_fun <- ifelse(statistic == "KS", GOF:::KS, GOF:::CvM)
-
-##   Y <- data[[Y_name]]
-##   b_times_x <- calc_beta_times_covariates(model = model)
-##   Y_hat <- calc_Y_hat(model = model)
-##   metric <- metric_fun(
-##     Rn1 = Rn1(
-##       Y = Y,
-##       Y_hat = Y_hat,
-##       beta_times_covariates = b_times_x
-##     )
-##   )
-
-
-##   data_boot <- data
-
-##   metric_boot <- sapply(seq_len(B), function(boot_iter) {
-##     data_boot[[Y_name]] <- simulate_Y()
-
-##     model_boot <- refit_model(model = model, data = data_boot)
-
-##     return(metric(model = model_boot))
-##   })
-
-##   out <- list(
-##     metric = metric,
-##     metric_boot = metric_boot,
-##     pvalue = mean(metric_boot > metric)
-##   )
-
-##   return(out)
-## }
-
 ##' Extracts the scalar product of beta with the covariates
 ##'
 ##' @param model fit inherited from class 'lm'.
@@ -110,6 +55,7 @@ Rn1 <- function(Y, Y_hat, order_beta_time_covariates) {
 ##' @param Rn1_vec marked empirical process evaluated at every
 ##'   position of the scalar of the estimated beta and the covariate
 ##' @return Cramer von Mises distance of \code{Rn1_vec}
+##' @export
 Rn1_CvM <- function(Rn1_vec) {
   ret <- mean(Rn1_vec^2)
   return(ret)
@@ -119,7 +65,99 @@ Rn1_CvM <- function(Rn1_vec) {
 ##' @param Rn1_vec marked empirical process evaluated at every
 ##'   position of the scalar of the estimated beta and the covariate
 ##' @return Kolmogorov-Smirnov distance of \code{Rn1_vec}
+##' @export
 Rn1_KS <- function(Rn1_vec) {
   ret <- max(abs(Rn1_vec))
+  return(ret)
+}
+##' Bootstraps the marked empirical process
+##'
+##' It simulates the dependent variable, refits the model and
+##' calculates the marked emp. process
+##' @param data that was used to fit \code{model}
+##' @param model that is bootstrapped
+##' @param order_beta_time_covariates order of the scalar product
+##'   of the estimated beta and the covariates.
+##' @param Y_name name of the dependent variables
+##' @param simulate_Y function that simulates the dependent
+##'   according to \code{model} and returns a vector
+##' @param refit_model function that refits the \code{model}
+##'   on the bootstrapped data
+##' @param B number of bootstraps
+##' @return list with \code{B} entries, every entry is a
+##'   marked empirical process
+resample_Rn1 <- function(data,
+                                model,
+                                order_beta_time_covariates,
+                                Y_name,
+                                simulate_Y,
+                                refit_model,
+                                B = 1000) {
+  ret <- lapply(
+    X = seq_len(B),
+    FUN = function(bootstrap_index) {
+      data[[Y_name]] <- simulate_Y()
+      fit_boot <- refit_model(model = model, data = data)
+      Rn1_boot <- Rn1(
+        Y = data[[Y_name]],
+        Y_hat = predict(object = fit_boot, type = "response"),
+        order_beta_time_covariates = order_beta_time_covariates)
+      return(Rn1_boot)
+    })
+  return(ret)
+}
+
+##' Performs a Goodness-of-fit test for GLMs or LSE based models.
+##'
+##' @param data that was used to fit \code{model}
+##' @param model that is bootstrapped
+##' @param type string. "parameteric" or "wild" in order
+##'   to perform the parameteric bootstrap for GLMs or
+##'   semi-parametric (wild) bootstrap for least square estimate
+##'   based model
+##' @param statistic function that is the functional for the
+##'   marked empirical process. The package provides \link{KS}
+##'   and \link{CvM}.
+##' @param B number of bootstraps
+##' @return a pvalue for the Goodness-of-fit of \code{model}
+##' @export
+gof_model <- function(data,
+                      model,
+                      type = c("parametric", "wild"),
+                      statistic,
+                      B) {
+  Y_name <- get_Y_name(model = model)
+  Y <- data[[Y_name]]
+  Y_hat <- predict(object = model, type = "response")
+  btx <- calc_beta_times_covariates(model = model)
+  o_btx <- order(btx)
+  Rn1_org <- Rn1(
+    Y = Y,
+    Y_hat = Y_hat,
+    order_beta_time_covariates = o_btx)
+
+  stat_org <- statistic(Rn1_org)
+  if (type == "parametric") {
+    sim_Y <- function() simulate(object = model)[, "sim_1"]
+  } else if (type == "wild") {
+    eps <- Y_hat - Y
+    N <- length(eps)
+    sim_Y <- function() {
+      r <- 2 * rbinom(n = N, size = 1, prob = 0.5) - 1
+      Y_hat + r * eps
+    }
+  }
+
+  Rn1_boot <- resample_Rn1(
+    data = data,
+    model = model,
+    order_beta_time_covariates = o_btx,
+    Y_name = Y_name,
+    simulate_Y = sim_Y,
+    refit_model = refit_model,
+    B = B
+  )
+  stat_boot <- sapply(Rn1_boot, statistic)
+  ret <- list(pvalue = mean(x = stat_org < stat_boot))
   return(ret)
 }
--- inst/tinytest/test_gof_model.R
+++ inst/tinytest/test_gof_model.R
@@ -77,3 +77,154 @@ calculate_kolmogorov_smirnov_for_marked_emp_process <- function() {
   expect_equal(Rn1_KS(e), max(abs(e)))
 }
 calculate_kolmogorov_smirnov_for_marked_emp_process()
+
+resampling_Rn1 <- function() {
+  set.seed(1)
+  d <- data.frame(y = rnorm(10), x = 1:10)
+  fit <- lm(y~x, data = d)
+  o_btx <- order(predict(fit, type = "response"))
+  sim_y <- function() {rnorm(n = 10)}
+  ref_m <- function(model, data) {
+    lm(y~x, data = data)
+  }
+  out <- resample_Rn1(
+    data = d,
+    model = fit,
+    order_beta_time_covariates = o_btx,
+    Y_name = "y",
+    simulate_Y = sim_y,
+    refit_model = ref_m,
+    B = 2)
+
+  set.seed(1)
+  rnorm(10)
+  d$y <- rnorm(10)
+  fit1 <- lm(y~x, data = d)
+  rn1_1 <- Rn1(
+    Y = d$y,
+    Y_hat = predict(fit1),
+    order_beta_time_covariates = o_btx)
+  d$y <- rnorm(10)
+  fit2 <- lm(y~x, data = d)
+  rn1_2 <- Rn1(
+    Y = d$y,
+    Y_hat = predict(fit2),
+    order_beta_time_covariates = o_btx)
+  expect_equal(out, list(rn1_1, rn1_2))
+
+}
+resampling_Rn1()
+
+gof_model_calculates_pvalue_KS <- function() {
+  d <- data.frame(y = 1:10, x1 = 1:10)
+  fit <- glm(
+    formula = y~x1,
+    data = d,
+    family = poisson())
+
+  mockery::stub(
+              where = gof_model,
+              what = 'Rn1',
+              how = c(1, -97, 19))
+  mockery::stub(
+              where = gof_model,
+              what = 'resample_Rn1',
+              how = lapply(1:100, function(i) sample(seq_len(i))))
+  out <- gof_model(
+    data = d,
+    model = fit,
+    type = "parametric",
+    statistic = Rn1_KS)
+  expect_equal(out$pvalue, 0.03)
+
+}
+gof_model_calculates_pvalue_KS()
+
+gof_model_calculates_pvalue_CvM <- function() {
+  d <- data.frame(y = 1:10, x1 = 1:10)
+  fit <- glm(y~x1, data = d, family = poisson())
+
+  rn1_stub <- c(1, -97, 19)
+  rn1_boot_stub <- lapply(1:100, function(i) sample(seq_len(i)))
+  mockery::stub(
+              where = gof_model,
+              what = 'Rn1',
+              how = rn1_stub)
+  mockery::stub(
+              where = gof_model,
+              what = 'resample_Rn1',
+              how = rn1_boot_stub)
+  out <- gof_model(
+    data = d,
+    model = fit,
+    type = "parametric",
+    statistic = Rn1_CvM)
+  expect_equal(
+    out$pvalue,
+    mean(Rn1_CvM(rn1_stub) < sapply(rn1_boot_stub, Rn1_CvM)))
+
+}
+gof_model_calculates_pvalue_CvM()
+
+gof_model_expect_small_pvalue <- function() {
+  set.seed(1)
+  X1 <- rnorm(100)
+  X2 <- rnorm(100)
+  d <- data.frame(
+    y = rpois(n = 100, lambda = exp(4 + X1 * 2 + X2 * 6)),
+    x1 = X1)
+  fit <- glm(y~x1, data = d, family = poisson())
+  out_p <- gof_model(
+    data = d,
+    model = fit,
+    type = "parametric",
+    statistic = Rn1_KS,
+    B = 100)
+  expect_equal(out_p$pvalue, 0)
+
+  X1 <- rnorm(100)
+  d <- data.frame(
+    y = rnorm(n = 100, mean = 4 + X1^2),
+    x1 = X1)
+  fit <- lm(y~x1, data = d)
+  out_w <- gof_model(
+    data = d,
+    model = fit,
+    type = "wild",
+    statistic = Rn1_CvM,
+    B = 100)
+
+  expect_equal(out_w$pvalue, 0)
+}
+gof_model_expect_small_pvalue()
+
+gof_model_expect_non_small_pvalue <- function() {
+  set.seed(1)
+  X1 <- rnorm(100)
+  d <- data.frame(
+    y = rpois(n = 100, lambda = exp(4 + X1 * 2)),
+    x1 = X1)
+  fit <- glm(y~x1, data = d, family = poisson())
+  out <- gof_model(
+    data = d,
+    model = fit,
+    type = "parametric",
+    statistic = Rn1_KS,
+    B = 100)
+  expect_equal(out$pvalue, 0.74)
+
+  X1 <- rnorm(100)
+  d <- data.frame(
+    y = rnorm(n = 100, mean = 4 + X1 + X1^2),
+    x1 = X1)
+  fit <- lm(y~x1 + I(x1^2), data = d)
+  out_w <- gof_model(
+    data = d,
+    model = fit,
+    type = "wild",
+    statistic = Rn1_CvM,
+    B = 100)
+
+  expect_equal(out_w$pvalue, 0.8)
+}
+gof_model_expect_non_small_pvalue()
