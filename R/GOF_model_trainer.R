cbbc576918d82c2545ca5efab98c355974e9b325
Marsel Scheer <scheer@freescience.de>
2020-10-09 19:36:11 +0000

    implement model trainer

---
+++ R/GOF_model_trainer.R
@@ -0,0 +1,42 @@
+##' R6 Class representing a trainer for fitting models
+##'
+##' R6 does not offer interfaces. Hence all methods
+##' are considered as abstract.
+GOF_model_trainer <- R6::R6Class(
+  classname = "GOF_model_trainer",
+  public = list(
+    ##' @description Abstract function refits the model to
+    ##'     a new data set
+    ##' @param model fitted model
+    ##' @param data used for refitting the model
+    ##' @return \code{model} refitted on \code{data}
+    refit = function(model, data) {
+      stop("Abstract method. Needs to be implemented")
+    }))
+
+
+##' Implements the "interface" GOF_model_trainer for
+##' for linear models
+GOF_lm_trainer <- R6::R6Class(
+  classname = "GOF_lm_trainer",
+  public = list(
+    ##' @description see \link{GOF_model_trainer}
+    ##' @param model see \link{GOF_model_trainer}
+    ##' @param data see \link{GOF_model_trainer}
+    ##' @return see \link{GOF_model_trainer}
+    refit = function(model, data) {
+      update(object = model, data = data)
+    }))
+
+##' Implements the "interface" GOF_model_trainer for
+##' for generalized linear models
+GOF_glm_trainer <- R6::R6Class(
+  classname = "GOF_glm_trainer",
+  public = list(
+    ##' @description see \link{GOF_model_trainer}
+    ##' @param model see \link{GOF_model_trainer}
+    ##' @param data see \link{GOF_model_trainer}
+    ##' @return see \link{GOF_model_trainer}
+    refit = function(model, data) {
+      update(object = model, data = data)
+    }))
