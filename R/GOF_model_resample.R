0356f7fdec13e6175fdde6d45f9d21f078e5f59a
Marsel Scheer <scheer@freescience.de>
2020-10-23 09:44:15 +0000

    make simulator and trainer private
    - no need to have them public
    - they are documented in the constructor

--- R/GOF_model_resample.R
+++ R/GOF_model_resample.R
@@ -3,15 +3,13 @@
 GOF_model_resample <- R6::R6Class(
   classname = "GOF_model_resample",
   public = list(
-    model_simulator = NULL,
-    model_trainer = NULL,
     ##' @description constructor.
     ##' @param gof_model_simulator an instance that implements \link{GOF_model_simulator}
     ##' @param gof_model_trainer an instance that implements \link{GOF_model_trainer}
     ##' @return No explicit return
     initialize = function(gof_model_simulator, gof_model_trainer) {
-      self$model_simulator <- gof_model_simulator
-      self$model_trainer <- gof_model_trainer
+      private$model_simulator <- gof_model_simulator
+      private$model_trainer <- gof_model_trainer
     },
     ##' @description resamples the dependent variable in \code{data} and refits
     ##'   \code{model} to that new data set
@@ -21,8 +19,12 @@ GOF_model_resample <- R6::R6Class(
     ##' @return a resampled version of \code{model}
     resample = function(model, data, y_name) {
       data_new <- data
-      data_new[[y_name]] <- self$model_simulator$resample_y(model = model)
-      ret <- self$model_trainer$refit(model = model, data = data_new)
+      data_new[[y_name]] <- private$model_simulator$resample_y(model = model)
+      ret <- private$model_trainer$refit(model = model, data = data_new)
       return(ret)
-    })
+    }),
+  private = list(
+    model_simulator = NULL,
+    model_trainer = NULL
+  )
 )
